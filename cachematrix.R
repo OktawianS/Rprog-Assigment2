e7312e62749c20fc58128e977014a62eee4809ff
OktawianS <oktawian88@gmail.com>
2014-07-25 15:45:59 -0500

    Assigment 2

---
+++ cachematrix.R
@@ -0,0 +1,45 @@
+## Test Assig 2 by OS - For this assignment, assume that the matrix supplied is always
+##invertible.
+##
+## Example invertible matrix:
+##
+##> x<- matrix(c(4,3,3,2),2,2)
+##> x
+##      [,1] [,2]
+##[1,]    4    3
+##[2,]    3    2
+##
+## `makeCacheMatrix`: This function creates a special "matrix" object
+## that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+## `cacheSolve`: This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+
+}
